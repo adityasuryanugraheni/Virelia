9859e7b671606bca5b476c19a25578efdb9da6de
adityasuryanugraheni <adityasuryanugraheni@gmail.com>
2026-05-08 09:42:41 +0700

    Proyek UAS

---
+++ app/build.gradle.kts
@@ -0,0 +1,95 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    //id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.virelia"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.virelia"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    // Firebase
+    //implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
+    //implementation("com.google.firebase:firebase-auth")
+    //implementation("com.google.firebase:firebase-firestore")
+    //implementation("com.google.firebase:firebase-messaging")
+
+    // Room
+    val room_version = "2.7.0"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+
+    // Compose
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+
+    implementation("androidx.activity:activity-compose:1.9.3")
+
+    implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+
+    // Icons
+    implementation("androidx.compose.material:material-icons-extended")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+
+    // Google Sign In
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
+
+    // Debug
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Test
+    testImplementation("junit:junit:4.13.2")
+}
